d013986bdef6bf3bf9bcbc8399d00e9cb6948f12
ankit496 <akankit496@gmail.com>
2023-11-17 14:03:00 +0530

    basic message sending and receiving

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ const server=http.createServer(app)
 const io=socketio(server)
 io.on('connection',(socket)=>{
     console.log('a user connected')
-    socket.on('from_client',()=>{
-        console.log('event coming from client')
+    socket.on('msg_send',(data)=>{
+        console.log(data)
+        io.emit('msg_rcvd',data)
     })
-    setInterval(()=>{
-        socket.emit('from_server')
-    },2000)
 })
 app.use('/',express.static(__dirname+'/public'))
 server.listen(3000,()=>{
